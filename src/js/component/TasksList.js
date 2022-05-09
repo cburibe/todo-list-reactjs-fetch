efc1c150ee0018e856c54215f02de145706e63d9
cburibe <curibepulgar@gmail.com>
2022-05-09 02:17:53 +0000

    Ready code

--- src/js/component/TasksList.js
+++ src/js/component/TasksList.js
@@ -1,66 +1,129 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Task } from "./Task";
 
 export function TaskList() {
+	const url = "https://assets.breatheco.de/apis/fake/todos/user/cburibe";
+
 	const [inputTask, setInputTask] = useState("");
-	const [toDoList, setToDoList] = useState([]);
+	const [list, setList] = useState([]);
 
-	const [taskDoneList, setTaskDoneList] = useState([]);
+	useEffect(() => {
+		getList();
+	}, []);
 
-	function addNewTask(task) {
-		if (inputTask.trim() !== "") {
-			setToDoList([
-				...toDoList,
-				{ id: Date.now(), text: task, done: false },
-			]);
+	function getInputValue(e) {
+		setInputTask(e.target.value);
+	}
+
+	async function getList() {
+		try {
+			let response = await fetch(url);
+
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			} else {
+				const fetchList = await response.json();
+
+				setList(fetchList);
+			}
+		} catch (e) {
+			console.error(`error from database -- ${e}`);
 		}
 	}
+
+	async function addTask(newTask) {
+		const newList = [
+			...list,
+			{
+				id: Date.now(),
+				label: newTask,
+				done: false,
+			},
+		];
+
+		const requestOptions = {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(newList),
+		};
+
+		try {
+			await fetch(url, requestOptions);
+			await getList();
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
 	function pressEnter(e) {
 		if (inputTask.trim() !== "") {
 			if (e.key === "Enter") {
-				addNewTask(inputTask);
+				addTask(inputTask);
 				setInputTask("");
 			}
 		}
 	}
 
-	function deleteTask(idTask, idTaskDone) {
-		if (idTask !== null) {
-			const newtoDoList = toDoList.filter((task) => task.id !== idTask);
-			setToDoList(newtoDoList);
-		} else if (idTaskDone !== null) {
-			const newDoneList = taskDoneList.filter(
-				(task) => task.id !== idTaskDone
-			);
-			setTaskDoneList(newDoneList);
+	async function addTaskDone(idDone) {
+		let newList = list.map((task) => {
+			if (task.id === idDone) {
+				task.done = true;
+			}
+
+			return task;
+		});
+
+		const requestOptions = {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(newList),
+		};
+
+		try {
+			await fetch(url, requestOptions);
+			await getList();
+		} catch (error) {
+			console.error(error);
 		}
 	}
 
-	//Ready Taks
-	function addTaskDone(idTaskDone) {
-		if (taskDoneList !== []) {
-			toDoList.filter((task) => {
-				if (task.id === idTaskDone) {
-					task.done = true;
-					setTaskDoneList([...taskDoneList, task]);
-					let indexTaskDone = toDoList.indexOf(task);
-					toDoList.splice(indexTaskDone, 1);
-					setToDoList(toDoList);
-				}
-			});
+	async function deleteTask(idTask) {
+		let newList = list.filter((task) => task.id !== idTask);
+
+		const requestOptions = {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(newList),
+		};
+
+		try {
+			await fetch(url, requestOptions);
+			await getList();
+		} catch (error) {
+			console.error(error);
 		}
 	}
-	function dontDone(idTask, done) {
-		if (done === true) {
-			taskDoneList.filter((task) => {
-				if (task.id === idTask) {
-					task.done = false;
-					setToDoList([...toDoList, task]);
-					let indexTask = taskDoneList.indexOf(task);
-					taskDoneList.splice(indexTask, 1);
-					setTaskDoneList(taskDoneList);
-				}
-			});
+
+	async function notDoneYet(taskId) {
+		let newList = list.map((task) => {
+			if (task.id === taskId) {
+				task.done = false;
+			}
+
+			return task;
+		});
+
+		const requestOptions = {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(newList),
+		};
+
+		try {
+			await fetch(url, requestOptions);
+			await getList();
+		} catch (error) {
+			console.error(error);
 		}
 	}
 
@@ -68,72 +131,96 @@ export function TaskList() {
 		<div className="container text-center mt-5 mb-5 pt-3 pb-5 d-flex justify-content-center rounded myListContainer">
 			<div className="p-0 m-0 myContainer">
 				<div className="row d-flex justify-content-center">
-					<h1 className="col-9 col-sm-10 col-md-10 col-lg-12 col-xl-12 mb-3 mt-2">
-						TODO LIST
-					</h1>
+					<h1 className="col-12 mb-3 mt-2">TODO LIST</h1>
 				</div>
 
 				<div className="row mb-4 d-flex justify-content-center">
 					<input
-						className="col-9 col-sm-10 col-md-10 col-lg-12 col-xl-12 border-0 rounded-pill text-center"
+						className="col-9 border-0 rounded-pill text-center"
 						type="text"
-						onChange={(e) => setInputTask(e.target.value)}
+						onChange={(e) => getInputValue(e)}
 						value={inputTask}
 						onKeyPress={(e) => pressEnter(e)}
-						placeholder="No tasks, add a task"
+						placeholder="Add a new task"
 						autoFocus
 					/>
 				</div>
 
 				<div className="row d-flex justify-content-center align-items-start">
-					<div className="d-flex flex-column col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-						<h4 className="col-8 col-sm-9 col-md-9 col-lg-12 col-xl-12 taskTitle">
-							To do tasks
-						</h4>
-						{toDoList.map((task) => {
-							return (
-								<Task
-									key={task.id}
-									id={task.id}
-									taskText={task.text}
-									done={task.done}
-									deleteTask={deleteTask}
-									addTaskDone={addTaskDone}
-									taskDoneList={taskDoneList}
-									dontDone={dontDone}
-								/>
-							);
-						})}
+					<div className="d-flex flex-column col-5 mr-1">
+						<h4 className="col-12 taskTitle">To do tasks</h4>
+						{list.filter((task) => task.done === false).length ===
+						0 ? (
+							<p className="col-12 mt-3 text-secondary">
+								Add a task
+							</p>
+						) : (
+							list
+								.filter((task) => task.done === false)
+								.map((task) => {
+									return (
+										<Task
+											key={task.id}
+											id={task.id}
+											taskText={task.label}
+											done={task.done}
+											deleteTask={deleteTask}
+											addTaskDone={addTaskDone}
+											notDoneYet={notDoneYet}
+										/>
+									);
+								})
+						)}
 
 						<div className="d-flex justify-content-start">
 							<div className="taskNum">
-								{toDoList.length} tasks left
+								{list.filter((task) => task.done === false)
+									.length > 0
+									? `${
+											list.filter(
+												(task) => task.done === false
+											).length
+									  } tasks left`
+									: ""}
 							</div>
 						</div>
 					</div>
 
-					<div className="d-flex flex-column col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-						<h4 className="col-8 col-sm-9 col-md-9 col-lg-12 col-xl-12 taskTitle">
-							Done tasks
-						</h4>
-						{taskDoneList.map((task) => {
-							return (
-								<Task
-									key={task.id}
-									id={task.id}
-									taskText={task.text}
-									done={task.done}
-									deleteTask={deleteTask}
-									addTaskDone={addTaskDone}
-									taskDoneList={taskDoneList}
-									dontDone={dontDone}
-								/>
-							);
-						})}
+					<div className="d-flex flex-column col-5 ml-1">
+						<h4 className="col-12 taskTitle">Done Tasks</h4>
+						{list.filter((task) => task.done === true).length ===
+						0 ? (
+							<p className="col-12 mt-3 text-secondary">
+								No completed tasks
+							</p>
+						) : (
+							list
+								.filter((task) => task.done === true)
+								.map((task) => {
+									return (
+										<Task
+											key={task.id}
+											id={task.id}
+											taskText={task.label}
+											done={task.done}
+											deleteTask={deleteTask}
+											addTaskDone={addTaskDone}
+											notDoneYet={notDoneYet}
+										/>
+									);
+								})
+						)}
 
-						<div className="d-flex justify-content-end">
+						<div className="d-flex justify-content-start">
 							<div className="taskNum">
-								{taskDoneList.length} tasks done
+								{list.filter((task) => task.done === true)
+									.length > 0
+									? `${
+											list.filter(
+												(task) => task.done === true
+											).length
+									  } tasks done`
+									: ""}
 							</div>
 						</div>
 					</div>
